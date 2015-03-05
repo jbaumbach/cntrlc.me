0b65427ef2e7f33993137a720db7bf54648cb9b3
John Baumbach <john.j.baumbach@gmail.com>
2015-03-05 02:17:18 +0000

    Toggleable logging, mostly commented

--- public/javascripts/ng-chat-app.js
+++ public/javascripts/ng-chat-app.js
@@ -1,10 +1,17 @@
+//
+// Front end Angular application
+//
 
 var chatApp = angular.module('chatApp', ['ngResource']);
 var checkLoginState;      // Required by FB SDK - ensure it's in scope at all times
+var DEBUG = false;        // set to "true" to enable console logging (development only)
 
+//
+// Enable our bearer token (e.g. session id) to be sent on every $http request
+//
 chatApp.service('Authorization', ['$http', function($http) {
   this.setSessionId = function(sessionId) {
-    console.log('setting Bearer token to header: ', sessionId);
+    DEBUG && console.log('setting Bearer token to header: ', sessionId);
     //
     // Set session ID in $http headers for all future requests
     //
@@ -13,50 +20,60 @@ chatApp.service('Authorization', ['$http', function($http) {
 }]);
 
 //
+// Utility functions
+//
+chatApp.service('GlobalFunctions', function() {
+  this.validateUrl = function(url) {
+    // useful variations: http://mathiasbynens.be/demo/url-regex
+    var urlRegex = /(((http|https):\/{2})+(([0-9a-z_-]+\.)+(aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cx|cy|cz|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mn|mn|mo|mp|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|nom|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ra|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|arpa)(:[0-9]+)?((\/([~0-9a-zA-Z\#\+\%@\.\/_-]+))?(\?[0-9a-zA-Z\+\%@\/&\[\];=_-]+)?)?))\b/gi;
+    return urlRegex.test(url);
+  };
+});
+
+//
 // Resource to load comments from the server
 //
 chatApp.factory('Comment', ['$resource', function($resource) {
   return $resource('/api/v1/comments');
 }]);
 
+//
+// Resource to manage the user
+//
 chatApp.factory('User', ['$http', 'Authorization', function($http, Authorization) {
   return {
     FBAuthResponse: null,
     info: null,
     login: function(callback) {
-      console.log('this: ', this);
-
+      
       $http.post(environment.host + '/loginfb', {
         FBAuthResponse: this.FBAuthResponse,
         info: this.info
       }).
         success(function(data, status, headers, config) {
-          
-          //
-          // Set session ID in $http headers for all future requests
-          //
-          //$http.defaults.headers.common.Authorization = 'Bearer ' + data.sessionId;
-
           Authorization.setSessionId(data.sessionId);
-
           callback(null, data.sessionId);
         }).
         error(function(data, status, headers, config) {
-          console.log('error: ', data, status);
-          callback('some error! ');
+          DEBUG && console.log('error: ', data, status);
+          callback('sorry, login error');
         });
     }
   };
 }]);
 
+//
+// App entry point.  Most code here is copied from the Facebook SDK sample
+// page, with some minor Angular-izing. 
+//
 chatApp.run(['$rootScope', '$window', 'User', '$http',
   function($rootScope, $window, User, $http) {
 
     $rootScope.$apply(function() {
       User.checkingLoggedIn = true;
     });
-    
-    console.log('app started!');
+
+    DEBUG && console.log('app started!');
 
     //
     // FB SDK stuff
@@ -64,14 +81,15 @@ chatApp.run(['$rootScope', '$window', 'User', '$http',
     // This is called with the results from from FB.getLoginStatus().
     function statusChangeCallback(response) {
 
-      // todo: solve flashing messages issue in UI (easy)
+      // todo: solve flashing messages issue in UI when user is already logged in (easy)
       
       $rootScope.$apply(function() {
         User.checkingLoggedIn = false;
       });
+
+      DEBUG && console.log('statusChangeCallback');
+      DEBUG && console.log(response);
       
-      console.log('statusChangeCallback');
-      console.log(response);
       // The response object is returned with a status field that lets the
       // app know the current login status of the person.
       // Full docs on the response object can be found in the documentation
@@ -79,7 +97,7 @@ chatApp.run(['$rootScope', '$window', 'User', '$http',
       if (response.status === 'connected') {
         // Logged into your app and Facebook.
         User.FBAuthResponse = response.authResponse;
-        testAPI();
+        continueLogin();
       } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
       } else {
@@ -134,20 +152,20 @@ chatApp.run(['$rootScope', '$window', 'User', '$http',
 
     // Here we run a very simple test of the Graph API after login is
     // successful.  See statusChangeCallback() for when this call is made.
-    function testAPI() {
-      console.log('Welcome!  Fetching your information.... ');
+    function continueLogin() {
+      DEBUG && console.log('Welcome!  Fetching your information.... ');
       FB.api('/me', function(response) {
-        console.log('Successful FB response for: ' + response.name);
+        DEBUG && console.log('Successful FB response for: ' + response.name);
 
         User.info = response;
-        console.log('Got user info!: ', User);
+        DEBUG && console.log('Got user info!: ', User);
 
         User.login(function(err, sessionId) {
           if (err) {
-            console.log('got error! ', err);
+            DEBUG && console.log('got error! ', err);
           } else {
             User.sessionId = sessionId;
-            console.log('good login! session id: ' + User.sessionId);
+            DEBUG && console.log('good login! session id: ' + User.sessionId);
             $rootScope.$broadcast('userLoggedIn');
           }
         })
@@ -156,16 +174,18 @@ chatApp.run(['$rootScope', '$window', 'User', '$http',
   }
 ]);
 
+//
+// Angular controller for the main screen
+//
 chatApp.controller('chatCtrl', function($scope, Comment, User) {
 
   $scope.$on('userLoggedIn', function() {
-    console.log('got broadcasted login event!');
+    DEBUG && console.log('got broadcasted login event!');
     $scope.checkingLoggedIn = false;
     $scope.isLoggedIn = true;
     $scope.user = User.info;
     
     getUserComments();
-    
     setUpSocket();
   });
   
@@ -175,12 +195,12 @@ chatApp.controller('chatCtrl', function($scope, Comment, User) {
     //
     // Get current comments from the server on initial page load
     //
-    console.log('getting comments...');
+    DEBUG && console.log('getting comments...');
     
     var serverComments = Comment.query(function success() {
       $scope.comments = serverComments;
     }, function error(err) {
-      console.log('error! ', err);
+      DEBUG && console.log('error! ', err);
     });
   }
 
@@ -189,10 +209,10 @@ chatApp.controller('chatCtrl', function($scope, Comment, User) {
     // Connect a socket to the server
     //
     var namespaceId = environment.host + '/' + User.sessionId;
-    console.log('connecting to NS: ' + namespaceId);
+    DEBUG && console.log('connecting to NS: ' + namespaceId);
     var socket = io.connect(namespaceId);
     
-    //Use 
+    // 
     // If we get a response, let's add it to the comments list
     //
     socket.on('addedComment', function (data) {
